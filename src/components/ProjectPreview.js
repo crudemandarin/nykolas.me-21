aa126507b3b60e799aa9ac29049f60f40e5aca7f
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2023-01-23 14:17:02 -0600

    123

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -8,19 +8,22 @@
  */
 function ProjectPreview({ project }) {
 	return (
-		<div className="w-[275px] h-[220px] p-3 bg-slate-200 rounded-md">
-			<img
-				className="h-[55px] mb-1"
-				src={project.img}
-				alt={`${project.title} Icon`}
-			/>
+		<div className="flex flex-col w-[275px] h-[220px] p-3 bg-slate-200 rounded-md">
+			<div>
+				<img
+					className="h-[55px] mb-1"
+					src={project.img}
+					alt={`${project.title} Icon`}
+				/>
+			</div>
 			<p className="mb-1 text-2xl font-bold">{project.title}</p>
-			<p className="mb-1">{project.shortDesc}</p>
+			<p>{project.shortDesc}</p>
+			<div className="flex-grow" />
 			<div className="flex">
 				{project.tags.map((tag) => (
 					<p
 						key={`${project.title}.${tag}`}
-						className="mr-3 text-green-500"
+						className="mr-3 text-green-500 font-semibold"
 					>
 						{tag}
 					</p>
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
 
 	return (
 		<section id="projects" className="flex justify-center mb-[200px]">
-			<div className="w-4/5">
+			<div className="w-4/5 max-w-[1200px]">
 				<h2 className="text-3xl mb-5 text-center">Projects</h2>
 				<div className="flex justify-center flex-wrap">
 					{projects.length === 0 ? (
